bf3f6f42cd8ba456830c52ba6f19e7cb1414c100
Koen <koenschnaar@gmail.com>
2020-02-11 11:56:38 +0100

    models gecreerd

---
+++ FitnessApp/FitnessApp/Models/PerformedExerciseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Models
+{
+    public class PerformedExerciseModel
+    {
+        public int PerformedExerciseId { get; set; }
+        public int ExerciseId { get; set; }
+        public int WorkoutFormId { get; set; }
+        public string Name { get; set; }
+        public int Sets { get; set; }
+        public List<RepsOfExerciseModel> Reps { get; set; }
+    }
+}
