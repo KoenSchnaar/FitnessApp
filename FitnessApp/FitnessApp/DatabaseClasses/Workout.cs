f0d26e4ff94b57a9e84d8fd68a9e8dd73dd61023
KoenSchnaar <koenschnaar@gmail.com>
2020-02-17 09:56:01 +0100

    Db workout refs3

---
+++ FitnessApp/FitnessApp/DatabaseClasses/Workout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessApp.DatabaseClasses
+{
+    public class Workout
+    {
+        public int WorkoutId { get; set; }
+    }
+}
